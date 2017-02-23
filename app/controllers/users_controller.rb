a11c59ce850a9d1c8e8636a5b4812210a82e4a97
frankburke333 <frank.burke@gmail.com>
2017-02-22 19:51:21 -0500

    add user page with all pledges.  Pledges are repeating right now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,34 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+
+    @rewards_id = []
+    @pledge_dollar_amounts = []
+    @projects = []
+
+    @user = User.find(params[:id])
+    @pledges = Pledge.where(user_id: params[:id])
+    @pledges.each do |pledge|
+      @projects << pledge.reward.project
+      #@pledge_dollar_amounts << pledge.dollar_amount
+    end
+    # Project.all.rewards.each do |reward|
+    #   reward.pledges.where(user_id: params[:id]).each do |pledge|
+    #     pledge.dollar_amount
+    #   end
+    # end
+
+    #@projects = Project.all
+    # @rewards_id.each do |reward_id|
+    #   @projects << Reward.find(reward_id).project
+    # end
+    # reward_id = Pledge.find_by(user_id:6).reward_id
+    # @project = Reward.find(reward_id).project
+   end
+
+
+
   private
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
