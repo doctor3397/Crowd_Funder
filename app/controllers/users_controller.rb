6a9663a55e79dbcf1a71cacb3cfda7dddf71a40b
frankburke333 <frank.burke@gmail.com>
2017-02-23 18:53:39 -0500

    Add project information into Users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       auto_login(@user)
-      redirect_back_or_to root
+      redirect_back_or_to root_path
     else
       render 'new'
     end
