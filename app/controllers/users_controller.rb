f6807f6da049669497d178152d359f26db143da0
doctor3397 <hs2546@columbia.edu>
2017-02-23 14:38:26 -0500

    Fix duplicates projects in user and project show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       auto_login(@user)
-      redirect_to projects_url
+      redirect_back_or_to root
     else
       render 'new'
     end
@@ -20,21 +20,9 @@ class UsersController < ApplicationController
     @pledges = Pledge.where(user_id: params[:id])
     @pledges.each do |pledge|
       @projects << pledge.reward.project
-      #@pledge_dollar_amounts << pledge.dollar_amount
     end
     @projects = @projects.uniq
-    # Project.all.rewards.each do |reward|
-    #   reward.pledges.where(user_id: params[:id]).each do |pledge|
-    #     pledge.dollar_amount
-    #   end
-    # end
 
-    #@projects = Project.all
-    # @rewards_id.each do |reward_id|
-    #   @projects << Reward.find(reward_id).project
-    # end
-    # reward_id = Pledge.find_by(user_id:6).reward_id
-    # @project = Reward.find(reward_id).project
    end
 
 
