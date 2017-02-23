fc3ad1a3b16c675bff2df611ab5cf6138304c3da
frankburke333 <frank.burke@gmail.com>
2017-02-23 17:22:17 -0500

    Add project_owner helper on project show page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  helper_method :project_owner
+
+  def project_owner(project)
+    return current_user.id == project.owner_id
+  end
+
+
   private
   def not_authenticated
     redirect_to login_path, alert: "Please login first"
