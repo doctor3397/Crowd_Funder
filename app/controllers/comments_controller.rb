6d55beffad784f90e091325f52aba6e39e1dfa88
doctor3397 <hs2546@columbia.edu>
2017-02-28 19:20:20 -0500

    Add Ajax for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,24 +1,36 @@
 class CommentsController < ApplicationController
   before_action :load_project
 
-
-
-
  def show
     @comment = Comment.find(params[:id])
   end
 
  def create
     @comment = Comment.new(comment_params)
-      @comment.project_id =  @project.id
-      @comment.user_id = current_user.id
+    @comment.project_id =  @project.id
+    @comment.user_id = current_user.id
 
+    if request.xhr?
+      @comment.save
+      @user = User.find(@comment.user_id)
 
+      respond_to do |format|
+        format.json {
+          render json:
+          { comment: @comment.comment,
+            user: @user.first_name,
+            created_at: @comment.created_at.strftime('%b %e, %l:%M %p')
+          }
+        }
+      end
 
-   if @comment.save
-      redirect_to projects_url, notice: 'comment created successfully'
     else
-      render 'projects/show'
+
+      if @comment.save
+        redirect_to projects_url, notice: 'comment created successfully'
+      else
+        render 'projects/show'
+      end
     end
 
  end
