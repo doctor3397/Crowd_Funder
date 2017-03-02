fb92da21d803721e939a170833070ace99092cca
doctor3397 <hs2546@columbia.edu>
2017-03-01 20:54:30 -0500

    Update seeds so projects ends in 1, 2 months

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,7 +28,7 @@ end
     description: Faker::Lorem.paragraph,
     goal: rand(100000),
     start_date: Time.now.utc - rand(60).days,
-    end_date: Time.now.utc + rand(10).days,
+    end_date: Time.now.utc + rand(30..60).days,
     category: ["Art", "Comics", "Crafts", "Dance", "Design", "Fashion", "Food", "Games", "Journalism", "Music", "Photography", "Publishing", "Technology", "Theater"].sample,
     image: ['1.jpg','2.jpg', '3.jpg','4.jpg', '5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg','11.jpg','12.jpg','13.jpg','14.jpg','15.png','16.jpg','17.png','18.png','19.png','20.png','21.png','22.png','23.jpg','24.jpg','25.png'][i]
     #photo: ,
