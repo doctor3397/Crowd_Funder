f2fc738e675a03124dbb2d8093c6e876bfebe64b
doctor3397 <hs2546@columbia.edu>
2017-03-02 18:17:08 -0500

    Update seeds

--- db/schema.rb
+++ db/schema.rb
@@ -1,75 +1,53 @@
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# Examples:
 #
-# It's strongly recommended that you check this file into your version control system.
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+Pledge.destroy_all
+Reward.destroy_all
+User.destroy_all
+Project.destroy_all
 
-ActiveRecord::Schema.define(version: 20170227045315) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+5.times do
+  User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.free_email,
+    password: 'password',
+    password_confirmation: 'password'
+  )
+end
 
-  create_table "comments", force: :cascade do |t|
-    t.integer  "project_id"
-    t.integer  "user_id"
-    t.text     "comment"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+25.times do |i|
+  p = Project.create!(
+    title: Faker::App.name,
+    owner_id: User.all.sample.id,
+    description: Faker::Lorem.paragraph,
+    goal: rand(100000),
+    start_date: Time.now.utc - rand(60).days,
+    end_date: Time.now.utc + rand(30..60).days,
+    category: ["Art", "Comics", "Crafts", "Dance", "Design", "Fashion", "Food", "Games", "Journalism", "Music", "Photography", "Publishing", "Technology", "Theater"].sample,
+    image: ['1.jpg','2.jpg', '3.jpg','4.jpg', '5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg','11.jpg','12.jpg','13.jpg','14.jpg','15.png','16.jpg','17.png','18.png','19.png','20.png','21.png','22.png','23.jpg','24.jpg','25.png'][i]
+    #photo: ,
+  )
 
-  create_table "pledges", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "reward_id"
-    t.float    "dollar_amount"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.index ["reward_id"], name: "index_pledges_on_reward_id", using: :btree
-    t.index ["user_id"], name: "index_pledges_on_user_id", using: :btree
-  end
 
-  create_table "projects", force: :cascade do |t|
-    t.string   "title"
-    t.text     "description"
-    t.integer  "goal"
-    t.datetime "start_date"
-    t.datetime "end_date"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "image"
-    t.integer  "owner_id"
-    t.string   "category"
-    t.string   "photo"
-  end
+ 5.times do
+   p.rewards.create!(
+     description: Faker::Superhero.power,
+     dollar_amount: rand(100),
+   )
+ end
+end
 
-  create_table "rewards", force: :cascade do |t|
-    t.integer  "project_id"
-    t.string   "description"
-    t.float    "dollar_amount"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "email",                                        null: false
-    t.string   "crypted_password",                             null: false
-    t.string   "salt",                                         null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "remember_me_token"
-    t.datetime "remember_me_token_expires_at"
-    t.boolean  "admin",                        default: false
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["remember_me_token"], name: "index_users_on_remember_me_token", using: :btree
-  end
-
-  add_foreign_key "pledges", "rewards"
-  add_foreign_key "pledges", "users"
+20.times do
+  r = Reward.all.sample
+  Pledge.create!(
+    user: User.all.sample,
+    reward: r,
+    dollar_amount: r.dollar_amount + rand(10)
+  )
 end
